6115289d69a2f69d20b4a6fa58cf984db6cec5ab
Vadim-n <vadim-nizhnevskiy@ya.ru>
2020-03-15 18:32:29 +0300

    Гитигнор. Описание категории

--- resources/js/components/front/category.vue
+++ resources/js/components/front/category.vue
@@ -2,17 +2,15 @@
     <div id="categoryPage">
         <preloader v-if="loading"></preloader>
         <div v-else>
+            <h1>{{category.name}}</h1>
+            <div v-html="category.description"></div>
             <div class="content-top" v-if="category.children && category.children.length">
-                <h1>{{category.name}}</h1>
-
                 <div v-for="(child, index) in category.children">
                     <item-block :item="child" :reference="child.alias"></item-block>
                 </div>
             </div>
 
             <div class="content-top" v-if="category.products && category.products.length">
-                <h1>Товары категории "{{category.name}}"</h1>
-
                 <div class="bottom-product">
                     <div v-for="(product, index) in category.products">
                         <item-block :item="product" :reference="category.alias + '/' + product.alias"></item-block>
